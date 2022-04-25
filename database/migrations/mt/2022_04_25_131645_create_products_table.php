0f5bda4a9745a2a3213434e244662c1edb76f109
Carlos Miranda <miranda.bouzas@gmail.com>
2022-04-25 15:36:00 +0200

    products table migration

---
+++ database/migrations/mt/2022_04_25_131645_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('sku', 6);
+            $table->string('name', 255);
+            $table->string('category', 100);
+            $table->integer('price', false, true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

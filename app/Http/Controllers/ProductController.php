557f1346be144e38d0e6f68ff019ba2fab7627b7
Carlos Miranda <miranda.bouzas@gmail.com>
2022-04-28 11:46:56 +0200

    Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Services\DiscountService;
+use App\Services\IdToStringService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $builder = Product::query();
+        $request->whenHas('category', function ($input) use ($builder) {
+            $builder->where('category', $input);
+        });
+        $request->whenHas('priceLessThan', function($input) use ($builder){
+            $builder->where('price', '<', $input);
+        });
+        $products = $builder->paginate(5)->getCollection();
+        $products = IdToStringService::idToString($products);
+        $products = DiscountService::applyDiscount($products);
+        return $products;
+    }
+}
